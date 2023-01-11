c0ae08338ca39d1f6674e535e33c78ec7c4c6f0a
markuslippo <71636573+markuslippo@users.noreply.github.com>
2023-01-11 13:40:38 +0200

    7.7

---
+++ part7/country-hook/src/App.js
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import { Country } from './components/Country'
+
+import { useField } from './hooks'
+import { useCountry } from './hooks'
+
+const App = () => {
+  const nameInput = useField('text')
+  const [name, setName] = useState('')
+  const country = useCountry(name)
+
+  const fetch = (e) => {
+    e.preventDefault()
+    setName(nameInput.value)
+  }
+
+  return (
+    <div>
+      <form onSubmit={fetch}>
+        <input {...nameInput} />
+        <button>find</button>
+      </form>
+
+      <Country country={country} />
+    </div>
+  )
+}
+
+export default App
